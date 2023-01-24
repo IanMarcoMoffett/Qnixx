ac2323d5f61d1a82fc9827fc96322be0fa6c1ffb
Ian Moffett <spurious.vector@gmail.com>
2023-01-24 03:02:14 -0500

    Style: Update interrupts.c

--- sys/amd64/interrupts.c
+++ sys/amd64/interrupts.c
@@ -1,12 +1,14 @@
 #include <amd64/interrupts.h>
 #include <amd64/idt.h>
 
-void register_interrupt(uint8_t vector, void(*isr)(void* sf))
+void 
+register_interrupt(uint8_t vector, void(*isr)(void* sf))
 {
   idt_set_desc(vector, isr, IDT_INT_GATE_FLAGS);
 }
 
-void register_exception(uint8_t vector, void(*isr)(void* sf))
+void 
+register_exception(uint8_t vector, void(*isr)(void* sf))
 {
   idt_set_desc(vector, isr, IDT_TRAP_GATE_FLAGS);
 }
