249d98e205f136094c3b753bf8fe15d1ab0be76b
Ian Moffett <spurious.vector@gmail.com>
2023-01-24 16:18:02 -0500

    Miscc: Update console.h

--- sys/include/tty/console.h
+++ sys/include/tty/console.h
@@ -4,6 +4,8 @@
 #include <stdarg.h>
 
 #define KERN_INFO "\033[1;37m[ \033[1;36mINFO \033[1;37m] "
+#define KERN_PANIC "\033[1;37m[ \033[1;31mPANIC \033[1;37m] "
+#define KERN_DEBUG "\033[1;37m[ \033[1;35mDEBUG \033[1;37m] "
 
 /* TODO: Allow formatting strings */
 void printk(const char* str);
