f5e41539b8a1038914bd5720e2277f113bf9302c
Ian Moffett <spurious.vector@gmail.com>
2023-01-27 03:27:44 -0500

    Misc: Update cdefs.h

--- sys/include/cdefs.h
+++ sys/include/cdefs.h
@@ -1,3 +1,8 @@
+/*
+ *  Description: Cdefs header.
+ *  Author(s): Ian Marco Moffett.
+ */
+
 #ifndef _INCLUDE_CDEFS_H_
 #define _INCLUDE_CDEFS_H_
 
@@ -8,5 +13,21 @@
 #define __packed      __attribute__((packed))
 #define __isr         __attribute__((interrupt))
 #define __asm         __asm__ __volatile__
+
+#ifdef __cplusplus
+#define __BEGIN_DECLS extern "C" {
+#define __END_DECLS   }
+#else
+#define __BEGIN_DECLS /* Empty */
+#define __END_DECLS   /* Empty */
+#endif
+
+#if defined(__STDC__) || defined(_AIX)              \
+     || (defined(__mips) && defined(_SYSTYPE_SVR4)) \
+     || defined(__cplusplus)
+#define __P(protos) protos
+#else
+#define __P(protos) ()
+#endif
 
 #endif
