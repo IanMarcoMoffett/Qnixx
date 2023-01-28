d9e90cdd224eccaeb26ee903b9b5a83166709e3c
Ian Moffett <spurious.vector@gmail.com>
2023-01-28 02:58:19 -0500

    Kernel: Update interrupts.h

--- sys/include/amd64/interrupts.h
+++ sys/include/amd64/interrupts.h
@@ -2,10 +2,14 @@
 #define _AMD64_INTERRUPTS_H_
 
 #include <types.h>
+#include <cdefs.h>
 
 void register_interrupt(uint8_t vector, void(*isr)(void* sf));
 void register_irq(uint8_t irq, void(*isr)(void* sf));
 void register_exception(uint8_t vector, void(*isr)(void* sf));
 uint8_t alloc_int_vector(void);
+
+/* Interrupt service routines */
+__isr void __lapic_timer_isr(void* sf);
 
 #endif
