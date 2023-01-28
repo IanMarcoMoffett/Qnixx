8d4b077654eac7557c0fd677ba33eeb1b2562a8a
Ian Moffett <spurious.vector@gmail.com>
2023-01-28 00:24:44 -0500

    Misc: Update CPU module.

--- sys/amd64/cpu.c
+++ sys/amd64/cpu.c
@@ -1,8 +1,28 @@
 #include <amd64/cpu.h>
+#include <amd64/smp.h>
 #include <amd64/interrupts.h>
 #include <sync/mutex.h>
+#include <sched/sched.h>
 
 static mutex_t goto_vec_lock = 0;
+
+static void core_set_flags(uint16_t flags)
+{
+  cpu_core_t* core = this_core();
+
+  mutex_acquire(&core->lock);
+  core->flags |= flags;
+  mutex_release(&core->lock);
+}
+
+static void core_clear_flags(uint16_t flags)
+{
+  cpu_core_t* core = this_core();
+
+  mutex_acquire(&core->lock);
+  core->flags &= ~(flags);
+  mutex_release(&core->lock);
+}
 
 void __amd64_for_each_core(void(*kroutine)(void* sf))
 {
@@ -13,4 +33,28 @@ void __amd64_for_each_core(void(*kroutine)(void* sf))
   lapic_send_ipi(0, goto_vector, LAPIC_ALL_INCLUDING_SELF); 
 
   mutex_release(&goto_vec_lock);
+}
+
+void
+irq_enter(void)
+{
+  core_set_flags(P_IRQ);
+}
+
+void
+irq_exit(void)
+{
+  core_clear_flags(P_IRQ);
+}
+
+void
+critical_enter(void)
+{
+  core_set_flags(P_EXEC_CRITICAL);
+}
+
+void
+critical_exit(void)
+{
+  core_clear_flags(P_EXEC_CRITICAL);
 }
--- sys/include/amd64/cpu.h
+++ sys/include/amd64/cpu.h
@@ -12,4 +12,34 @@ void __amd64_for_each_core(void(*kroutine)(void* sf));
 
 int __amd64_enable_sse(void);
 
+
+/*
+ *  Marks the processor as
+ *  currently handling an IRQ.
+ */
+
+void irq_enter(void);
+
+/*
+ *  Marks the processor
+ *  as no longer handling an IRQ.
+ */
+
+void irq_exit(void);
+
+/*
+ *  Marks the processor as executing
+ *  critical code.
+ */
+
+void critical_enter(void);
+
+/*
+ *  Marks the processor as
+ *  no longer executing
+ *  critical code.
+ */
+
+void critical_exit(void);
+
 #endif
