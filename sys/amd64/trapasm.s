ea976c8527b86772dbc124d3b03b7c3f56ab615c
Ian Moffett <spurious.vector@gmail.com>
2023-01-28 02:56:40 -0500

    Kernel: Add trap handling module.

---
+++ sys/amd64/trapasm.s
@@ -0,0 +1,41 @@
+.globl trap_entry
+
+trap_entry:
+  push %gs
+  push %fs
+  push %r15
+  push %r14
+  push %r13
+  push %r12
+  push %r11
+  push %r10
+  push %r9
+  push %r8
+  push %rax
+  push %rcx
+  push %rdx
+  push %rbx
+  push %rsi
+  push %rdi
+
+  mov %rsp, %rdi
+  call trap
+trap_end:
+  pop %rdi
+  pop %rsi
+  pop %rbx
+  pop %rdx
+  pop %rcx
+  pop %rax
+  pop %r8
+  pop %r9
+  pop %r10
+  pop %r11
+  pop %r12
+  pop %r13
+  pop %r14
+  pop %r15
+  pop %fs
+  pop %gs
+  add $8, %rsp
+  iretq
