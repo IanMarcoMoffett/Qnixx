e81781acf7d97adfbf2f8d844cb1c9c3400e64b5
Ian Moffett <spurious.vector@gmail.com>
2023-01-28 04:35:34 -0500

    Kernel: Use regular LAPIC for now.

--- sys/acpi/acpi.c
+++ sys/acpi/acpi.c
@@ -9,6 +9,10 @@
 #include <limine.h>
 #include <mm/vmm.h>
 #include <tty/console.h>
+#include <lib/assert.h>
+
+#define APIC_TYPE_IO_APIC               1
+#define APIC_TYPE_INTERRUPT_OVERRIDE    2
 
 static volatile struct limine_rsdp_request rsdp_req = {
   .id = LIMINE_RSDP_REQUEST,
@@ -16,7 +20,31 @@ static volatile struct limine_rsdp_request rsdp_req = {
 };
 
 static acpi_rsdt_t* rsdt = NULL;
+static acpi_madt_t* madt = NULL;
 static size_t rsdt_entry_count = 0;
+
+void* g_ioapic_mmio_base = NULL;
+void* g_lapic_mmio_base  = NULL;
+
+static void
+parse_madt(void)
+{
+  g_lapic_mmio_base = (void*)(uint64_t)(madt->lapic_addr);
+  uint8_t* cur = (uint8_t*)(madt + 1);
+  uint8_t* end = (uint8_t*)madt + madt->header.length;
+  while (cur < end)
+  {
+    apic_header_t* apic_header = (apic_header_t*)cur;
+    if (apic_header->type == APIC_TYPE_IO_APIC)
+    {
+      io_apic_t* ioapic = (io_apic_t*)cur;
+      g_ioapic_mmio_base = (void*)(uint64_t)ioapic->io_apic_addr;
+      printk(KERN_INFO "IOAPIC: MMIO base: %x\n", g_ioapic_mmio_base);
+    }
+
+    cur += apic_header->length;
+  }
+}
 
 static uint8_t
 do_checksum(const acpi_header_t* header) 
@@ -30,6 +58,7 @@ do_checksum(const acpi_header_t* header)
 
   return sum % 0x100 == 0;
 }
+
 
 void*
 acpi_query(const char* query)
@@ -46,6 +75,29 @@ acpi_query(const char* query)
   return NULL;
 }
 
+uint16_t
+acpi_remap_irq(uint8_t irq)
+{
+  uint8_t* cur = (uint8_t*)(madt + 1);
+  uint8_t* end = (uint8_t*)madt + madt->header.length;
+
+  while (cur < end) 
+  {
+    apic_header_t* apic_header = (apic_header_t*)cur;
+    if (apic_header->type == APIC_TYPE_INTERRUPT_OVERRIDE) 
+    {
+      apic_interrupt_override_t* intr_override = (apic_interrupt_override_t*)cur;
+      if (intr_override->source == irq) 
+      {
+        return intr_override->interrupt;
+      }
+    }
+
+    cur += apic_header->length;
+  }
+
+  return irq;
+}
 
 void
 acpi_init(void)
@@ -56,4 +108,8 @@ acpi_init(void)
   /* Fetch the RSDT */
   rsdt = (acpi_rsdt_t*)((uint64_t)rsdp->rsdtaddr + VMM_HIGHER_HALF);
   rsdt_entry_count = (rsdt->header.length - sizeof(rsdt->header)) / 4;
+
+  madt = acpi_query("APIC");
+  assert(madt != NULL); 
+  parse_madt();
 }
--- sys/include/acpi/acpi.h
+++ sys/include/acpi/acpi.h
@@ -1,8 +1,13 @@
 #ifndef _ACPI_H_
 #define _ACPI_H_
 
+#include <types.h>
 
 void* acpi_query(const char* query);
 void acpi_init(void);
+uint16_t acpi_remap_irq(uint8_t irq);
+
+extern void* g_ioapic_mmio_base;
+extern void* g_lapic_mmio_base;
 
 #endif
